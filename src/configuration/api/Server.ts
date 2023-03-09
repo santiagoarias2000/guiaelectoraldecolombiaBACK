7fd12b3d08d0a28aee3edec7e3d0336b82036126
santiagoarias2000 <santiago.ariasm@gmail.com>
2023-03-09 17:39:44 -0500

    funcionado en el puerto 443 con https

--- src/configuration/api/Server.ts
+++ src/configuration/api/Server.ts
@@ -7,33 +7,44 @@ import PresidenciaRoute from "../../routes/PresidenciaRoute";
 import camaraRoute from "../../routes/CamaraRoute";
 import municipioRoute from "../../routes/MunicipioRoute";
 import departamentoRoute from "../../routes/DepartamentoRoute";
+var fs = require("fs");
+var https = require("https");
 
-class Server{
-    public app: express.Application; 
 
-    constructor(){
-        this.app = express();
-        this.startSetting();
-        this.activeRoute();
-    }
-    public startSetting(): void{
-        this.app.set("PORT", 3123);
-        this.app.use(cors());
-        this.app.use(morgan("dev"));
-        this.app.use(express.json({limit:"100mb"}));
-        this.app.use(express.urlencoded({extended:true}));
-    }
-    public activeRoute():void{
-        this.app.use('/api/public/camara',camaraRoute);
-        this.app.use('/api/public/municipio',municipioRoute);
-        this.app.use('/api/public/departamento',departamentoRoute);
-        this.app.use('/api/public/senado',SenadoRoute);
-       this.app.use('/api/public/presidencia',PresidenciaRoute);
-    }
-    public start(): void {
-        this.app.listen(this.app.get("PORT"),()=>{
-            console.log("It works", this.app.get("PORT"));
-        });
-    }
+class Server {
+  public app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.startSetting();
+    this.activeRoute();
+  }
+  public startSetting(): void {
+    this.app.set("PORT", 443);
+    this.app.use(cors());
+    this.app.use(morgan("dev"));
+    this.app.use(express.json({ limit: "100mb" }));
+    this.app.use(express.urlencoded({ extended: true }));
+  }
+  public activeRoute(): void {
+    this.app.use("/api/public/camara", camaraRoute);
+    this.app.use("/api/public/municipio", municipioRoute);
+    this.app.use("/api/public/departamento", departamentoRoute);
+    this.app.use("/api/public/senado", SenadoRoute);
+    this.app.use("/api/public/presidencia", PresidenciaRoute);
+  }
+
+  public start(): void {
+    const options = {
+        key: fs.readFileSync('private.key'),
+        cert: fs.readFileSync('server.crt')
+    };
+
+    const server = https.createServer(options, this.app);
+
+    server.listen(443, () => {
+      console.log("Servidor HTTPS iniciado");
+    });
+  }
 }
 export default Server;
